d1fa47aa43fee107247af80167514e1c4010fb15
Nahom Wm <dbu1601542@dbu.edu.et>
2025-12-16 12:02:44 +0300

    final exercise 2.5

--- part2/courseinfo-part2/src/App.jsx
+++ part2/courseinfo-part2/src/App.jsx
@@ -1,40 +1,4 @@
-const Header = ({course}) => <h1>{course.name}</h1>
-
-const Content = ({parts}) => {
-  return(<div>
-        {parts.map(part=>
-          <Part key={part.id} part={part}/>
-        )}
-  </div>)
-}
-
-const Part = ({part
-}) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Total = ({parts}) => {
-  return(<p><b>total of 
-    {parts.reduce((sumUp,part)=>
-    sumUp+part.exercises,0
-    )} exercises
- </b> </p>)
-}
-
-const Course=({course})=>{
-  return(
-    <div>
-
-  <Header course={course}/>
-  <Content parts={course.parts}/>
-  <Total parts={course.parts}/>
-    </div>
-)
-}
-
-
+import Course from "./components/Course"
 const App = () => {
    const courses = [
     {
