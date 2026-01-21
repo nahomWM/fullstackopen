78993b04ff5bef67f7642bb636d4f45a25e0e655
Nahom Wm <dbu1601542@dbu.edu.et>
2026-01-21 11:05:03 +0300

    add exercise 3.4

--- part3/phonebook-part3/index.js
+++ part3/phonebook-part3/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-const data = require("./data.json");
+let data = require("./data.json");
 app.get("/api/persons", (request, response) => {
   response.json(data);
 });
-const length = data.length;
 
 app.get("/info", (request, response) => {
   response.send(`
-    <p>Phonebook has info for ${length} people</p>
+    <p>Phonebook has info for ${data.length} people</p>
     <p>${new Date()}</p>
   `);
 });
+
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   const datamain = data.find((data) => data.id === id);
@@ -20,6 +20,11 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  data = data.filter((data) => data.id !== id);
+  response.status(204).end();
 });
 const PORT = 3001;
 app.listen(PORT, () => {
