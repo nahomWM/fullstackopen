0eaf374521ac6a48279d8bb079c6f88d02527e87
Nahom Wm <dbu1601542@dbu.edu.et>
2026-01-21 10:46:44 +0300

    add exercise 3.2

--- part3/phonebook-part3/index.js
+++ part3/phonebook-part3/index.js
@@ -4,6 +4,14 @@ const data = require("./data.json");
 app.get("/api/persons", (request, response) => {
   response.json(data);
 });
+const length = data.length;
+
+app.get("/info", (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${length} people</p>
+    <p>${new Date()}</p>
+  `);
+});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
