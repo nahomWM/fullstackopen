e64e5cb662a1463619f8f18490707c3897fbd4a1
Nahom Wm <dbu1601542@dbu.edu.et>
2026-01-22 10:12:15 +0300

    add exercise 3.11

--- part2/phonebook/src/services/phonebooks.js
+++ part2/phonebook/src/services/phonebooks.js
@@ -1,23 +1,17 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
-
+const baseUrl = "http://localhost:3001/api/persons";
 const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  return axios.get(baseUrl).then((response) => response.data);
 };
-
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
+  return axios.post(baseUrl, newObject).then((response) => response.data);
 };
 const deleteSingle = (id) => {
   return axios.delete(`${baseUrl}/${id}`);
 };
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((res) => res.data);
+  return axios.put(`${baseUrl}/${id}`, newObject).then((res) => res.data);
 };
-
 export default {
   getAll,
   create,
