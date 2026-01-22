9375341602926ce3231c922cd5be94aabe6d79b9
nahom weldemedhin <dbu1601542@dbu.edu.et>
2026-01-22 09:49:07 +0300

    add PORT as env

--- part3/phonebook-part3/index.js
+++ part3/phonebook-part3/index.js
@@ -57,7 +57,7 @@ app.post("/api/persons", (request, response) => {
   data = data.concat(person);
   response.json(person);
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
